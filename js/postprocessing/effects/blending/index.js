3a923d2df98380c315092df1805e0f67199e089e
Anson Li <hello@ansonli.ca>
2018-11-30 16:26:28 -0700

    Revert "major changes to adapt three"
    This reverts commit db0e23db245414df06ea6491324e4cbe50804fa3.

--- js/postprocessing/effects/blending/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * A collection of blend functions.
- *
- * @module postprocessing/blending
- */
-
-export { BlendFunction } from "./BlendFunction.js";
-export { BlendMode } from "./BlendMode.js";
--- js/postprocessing/effects/glsl/texture/shader.vert
+++
@@ -1,9 +0,0 @@
-uniform float scale;
-
-varying vec2 vUv2;
-
-void mainSupport() {
-
-	vUv2 = uv * vec2(aspect, 1.0) * scale;
-
-}
