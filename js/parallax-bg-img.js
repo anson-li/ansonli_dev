40df26f88d5e47615411a041022182f65c789521
Anson Li <hello@ansonli.ca>
2020-03-25 23:58:31 -0600

    Changed padding style and removed parallax effect

--- js/parallax-bg-img.js
+++ js/parallax-bg-img.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function (e) {
   var scrolled = window.pageYOffset;
   const background = document.querySelector('.bg-img');
-  background.style.top = (scrolled * 0.5) - 150 + 'px';
+  background.style.top = (scrolled * 0) - 150 + 'px';
 });
